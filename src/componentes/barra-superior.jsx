13ddfa4731ddd614fcb16baef454298fe15e3ae2
josué <josue.elias.f@gmail.com>
2023-02-28 15:31:04 -0300

    css

--- src/componentes/barra-superior.jsx
+++ src/componentes/barra-superior.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import './componentes.css'
+import '../index.css'
 import filter_fill from '../assets/filter_fill.svg' 
 
 /*
  * Barra superior do site
- * 
 */ 
 
 function BarraSuperior() {
     return(
         <div class="barra-superior">
+
             {/*Barra de navegação lateral*/}
-            <div class="items-barra-superior" id="item1">1</div> 
+            <div class="items-barra-superior flex0" id="item1">1</div> 
 
-            {/*Barra de pesquisa*/}
-            <div class="items-barra-superior" id="barra-pesquisa">
-                <input type="text" placeholder="Pesquisar" id="pesquisar"></input>
-
-                <button id="btn-filtrar" >
-                    <img src={filter_fill} className="icon" alt="filtrar"></img>
-                </button>
-            </div>
+            <BarraDePesquisa></BarraDePesquisa>
 
             {/*Modo escuro*/}
             <div class="items-barra-superior">4</div>
 
             {/*Perfil*/}
             <div class="items-barra-superior">5</div>
+        </div>
+    )
+}
+
+function BarraDePesquisa() {
+    return (
+        <div class="items-barra-superior" id="barra-pesquisa">
+            <input type="text" placeholder="Pesquisar" id="pesquisar"></input>
+            <button id="btn-filtrar" >
+                <img src={filter_fill} className="icon" alt="filtrar"></img>
+            </button>
         </div>
     )
 }
--- src/test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Test() {
-    return(
-        <p>Helo World!</p>
-    )
-}
-
-export default Test
