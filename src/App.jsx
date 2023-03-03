c1e6e1a9a1db1fff6afc0bb548652bed9e897665
josué <josue.elias.f@gmail.com>
2023-03-03 14:38:57 -0300

    trabalhando na barra superior

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import BarraSuperior from './componentes/barra-superior.jsx'
 
 function App() {
 
+  let stilo = {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "red"
+  }
+
   return (
     <div className="App">
-      <BarraSuperior />
+      <div>
+        <BarraSuperior />
+      </div>
     </div>
   )
 }
--- src/componentes/barra-superior.jsx
+++ src/componentes/barra-superior.jsx
@@ -1,33 +1,63 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './componentes.css'
 import '../index.css'
 import filter_fill from '../assets/filter_fill.svg' 
+import menu_icon from '../assets/menu_icon.svg'
+import conta_icon from '../assets/conta_icon.svg'
+import logo from '../assets/logo.svg'
 
 /*
  * Barra superior do site
 */ 
 
 function BarraSuperior() {
+
     return(
         <div class="barra-superior">
 
-            <MenuNav></MenuNav>
+
+            <ItemNav icon={menu_icon} alt="test" link="#">
+            </ItemNav>
+            <ItemNav icon={conta_icon} alt="test" link="#" />
 
             <BarraDePesquisa></BarraDePesquisa>
 
-            {/*Modo escuro*/}
-            <div class="items-barra-superior">4</div>
+            <ItemNav icon={logo} alt="test" link="../index.html" />
 
-            {/*Perfil*/}
-            <div class="items-barra-superior">5</div>
         </div>
     )
 }
 
-function MenuNav() {
-    return (
-        <div className='flex1 menu-nav'>
+function ItemNav(props) {
+    const [itemAberto, setNav] = useState(false)
 
+    return (
+        <div className='menu-nav'>
+            <ul>
+                <a href={props.link} onClick={() => {setNav(!itemAberto)}}>
+                    <img src={props.icon} alt={props.alt}  className="icon"></img>
+                </a>
+
+                {itemAberto && props.children}
+            </ul>
+        </div>
+        
+    )
+}
+
+function DropNav() {
+    function ItemDropNav() {
+        return (
+            <a href='#' className='item-drop-nav'>
+                {props.children}
+            </a>
+
+        )
+    }
+
+    return (
+        <div className='drop-nav'>
+            <ItemDropNav>   </ItemDropNav>
         </div>
     )
 }
