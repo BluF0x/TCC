09f6c6ff1adfbff6a003ce5a49db6834b2109adc
Josue Elias <BluF0x>
2023-04-05 14:53:05 -0300

    restruturação pra cliente/servidor

--- client/src/componentes/SelectTenis/SelectTenis.jsx
+++ client/src/componentes/SelectTenis/SelectTenis.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import "./select-tenis.css"
 
 function SelectTenis(props) {
-    function handleScroll(e) {
-        console.log("scroll")
-        console.log(e.currentTarget);
-
-    }
 
     return(
         <div className="select-container">
             <h1>{props.titulo}</h1>
-            <div className="cards" onScroll={handleScroll}>
+            <div className="cards" >
                 {props.children}
             </div>
         </div>
