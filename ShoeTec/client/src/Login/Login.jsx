ea981aac4e06f94379d5c53ff1e5e51d926c9196
ArthurBC07 <arthurcardososjs@gmail.com>
2023-04-10 22:30:46 -0300

    Formulário - Criar Conta

--- ShoeTec/client/src/Login/Login.jsx
+++ ShoeTec/client/src/Login/Login.jsx
@@ -7,6 +7,15 @@ import './login.css';
 export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nome, setNome] = useState("");
+  const [pais, setPais] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [masculino, setMasculino] = useState("");
+  const [feminino, setFeminino] = useState("");
+  const [outro, setOutro] = useState("");
+  const [naoidentificado, setNaoIdentificado] = useState("");
   const [estadoLogin, setEstadoLogin] = useState(true)
 
   function loginOuCriar() {
@@ -80,7 +89,17 @@ export function Login() {
                     <img src={SneakerTecLogo} alt="Logo SneakerTec"></img>
                 </span>
 
-                <div className="wrap-input">
+                <div className="wrap-input-cadastro">
+                    <input 
+                    className = {nome !== "" ? 'has-val input' : 'input'}
+                    type = "text"
+                    value = {nome}
+                    onChange = {e => setNome(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="Nome Completo"></span>
+                </div>
+
+                <div className="wrap-input-cadastro">
                     <input 
                     className = {email !== "" ? 'has-val input' : 'input'}  
                     type="email"
@@ -90,7 +109,37 @@ export function Login() {
                     <span className="focus-input" data-placeholder="Email"></span>
                 </div>
 
-                <div className="wrap-input">
+                <div className="wrap-input-cadastro">
+                    <input 
+                    className = {pais !== "" ? 'has-val input' : 'input'}
+                    type = "text"
+                    value = {pais}
+                    onChange = {e => setPais(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="País"></span>
+                </div>
+
+                <div className="wrap-input-cadastro">
+                    <input 
+                    className = {estado !== "" ? 'has-val input' : 'input'}
+                    type = "text"
+                    value = {estado}
+                    onChange = {e => setEstado(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="Estado"></span>
+                </div>
+
+                <div className="wrap-input-cadastro">
+                    <input 
+                    className = {cidade !== "" ? 'has-val input' : 'input'}
+                    type = "text"
+                    value = {cidade}
+                    onChange = {e => setCidade(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="Cidade"></span>
+                </div>
+
+                <div className="wrap-input-cadastro">
                     <input 
                     className = {password !== "" ? 'has-val input' : 'input'}
                     type = "password"
@@ -98,6 +147,55 @@ export function Login() {
                     onChange = {e => setPassword(e.target.value)}
                     />
                     <span className="focus-input" data-placeholder="Password"></span>
+                </div>
+
+                <div className="wrap-input-cadastro">
+                    <input 
+                    className = {confirmPassword !== "" ? 'has-val input' : 'input'}
+                    type = "password"
+                    value = {confirmPassword}
+                    onChange = {e => setConfirmPassword(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="Confirm password"></span>
+                </div>
+
+                <div className="wrap-input-cadastro">
+                    <h3 className='titulo-genero'>Gênero:</h3>
+                        <input 
+                        className = 'input-btn'
+                        id="masc"
+                        name="genero"
+                        type = "radio"
+                        value = {masculino}
+                        />
+                        <label className="label-radio"  for="masc">Masculino</label>
+
+                        <input 
+                        className = 'input-btn'
+                        id="fem"
+                        name="genero"
+                        type = "radio"
+                        value = {feminino}
+                        />
+                        <label className="label-radio" for="fem">Feminino</label>
+
+                        <input 
+                        className = 'input-btn'
+                        id="outro"
+                        name="genero"
+                        type = "radio"
+                        value = {outro}
+                        />
+                        <label className="label-radio" for="outro">Outro</label>
+
+                        <input 
+                        className = 'input-btn'
+                        id="naoidentificado"
+                        name="genero"
+                        type = "radio"
+                        value = {naoidentificado}
+                        />
+                        <label className="label-radio" for="naoidentificado">Não quero identificar</label>
                 </div>
 
                 <div className="container-login-form-btn">
