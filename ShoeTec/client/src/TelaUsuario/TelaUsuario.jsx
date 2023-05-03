e4ceb2cd3f3085f6e9ba23a522d1e493a642d809
ArthurBC07 <arthurcardososjs@gmail.com>
2023-05-03 18:10:23 -0300

    começo da tela do usuário

--- ShoeTec/client/src/TelaUsuario/TelaUsuario.jsx
+++ ShoeTec/client/src/TelaUsuario/TelaUsuario.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BarraSuperior from '../componentes/BarraSuperior.jsx/barra-superior.jsx'
+import Arthur from '../assets/imgs/arthur.jpg';
 import './tela-usuario.css'
 
 export function TelaUsuario() {
@@ -18,7 +19,32 @@ export function TelaUsuario() {
     function PerfilUsuario() {
         return(
             <div className="perfil-container">
-                Perfil
+                <div className="perfil-foto">
+                <img className="user-foto" src={Arthur} alt="Foto Usuário"></img>
+                </div>
+                <div className="perfil-cardname">
+                    <h1 className="perfil-nome">
+                        Arthur Bauer Cardoso
+                    </h1>
+                </div>
+                
+                <div className="perfil-icons">
+                <img className="favicon" src="https://img.icons8.com/ios-filled/25/000000/basketball.png"/>
+                <img className="favicon" src="https://img.icons8.com/ios-filled/25/000000/basketball.png"/>
+                <img className="favicon" src="https://img.icons8.com/ios-filled/25/000000/basketball.png"/>
+                <img className="favicon" src="https://img.icons8.com/ios-filled/25/000000/basketball.png"/>
+                <img className="favicon" src="https://img.icons8.com/ios-filled/25/000000/basketball.png"/>
+                </div>
+
+                <div className="perfil-descricao">
+                    <p className="perfil-descricao-p"> 1º lugar 10km Giassi Sombrio 2020 sub-24</p>
+                </div>
+
+                <div className="perfil-edit">
+                <button type="button" className="btn-edit-profile">Editar Perfil</button>
+                </div>
+                
+                
             </div>
         )
     }
